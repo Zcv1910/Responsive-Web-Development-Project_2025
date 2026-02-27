11f26897dbd81fb8e78291c5a7af3e16e86d2d52
Zanden Choo Valens <zandenchoo19@gmail.com>
2026-02-27 12:11:13 +0800

    Initial commit.

---
+++ Home_Page.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poolers</title>
+</head>
+
+<body>
+    <main class="container">
+        <nav>  <!-- this is the websites navigation -->
+            <li> 
+                <ul>something</ul>
+                <ul>something</ul>
+                <ul>something</ul>
+                <ul>something</ul>
+                <ul>something</ul>
+            </li>
+        </nav>
+
+        <br>
+
+        <section class="userSelection">
+            <!-- this is where the different drivers are displayed  -->
+        </section>
+
+        <footer class="footer">
+            <!-- website footer -->
+            <img src="" alt="">
+
+            <div class="footerExplore">
+                <h5>Explore</h5>
+
+                <li>
+                    <ul>Home</ul>
+                    <ul>Share a Ride</ul>
+                    <ul>Request a Ride</ul>
+                </li>
+            </div>
+
+            <div class="footerUserDashboard">
+                <h5>User Dashboard</h5>
+
+                <li>
+                    <ul>Driver Dashboard</ul>
+                    <ul>Ride History</ul>
+                </li>
+            </div>
+
+            <div class="supportUs">
+                <h5>Support Us</h5>
+
+                <li>
+                    <ul>Contact Us</ul>
+                    <ul>About Us</ul>
+                    <ul>Book a Ride</ul>
+                </li>
+            </div>
+
+            <p>© 2026 CarpoolConnect. All rights reserved. <br>
+                This website is developed for educational purposes only.
+            </p>
+        </footer>
+
+    </main>
+</body>
+</html>
---
+++ Request_Page.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ride Request</title>
+</head>
+
+<body>
+    <button id="backBtn" onclick="history.go(-1)">back</button>
+
+    <section class="driverInfo">
+        <img src="" alt="">
+
+        <h3>Driver Name: </h3>
+
+        <h3>Number of Rides: </h3>
+    </section>    
+
+    <section class="rideLocation">
+        <h3>Journey Starts: </h3>
+
+        <h3>Journey Ends:  </h3>
+
+        <h3>Journey Start Time:  </h3>
+
+        <h3>Number of Passengers: </h3>
+    </section>
+
+    <iframe src="" frameborder="0"></iframe>
+
+    <section class="vehicleDescription">
+        <h1>Driver Vehicle</h1>
+
+        <h3>Number of Rides: </h3>
+
+        <h3>Number of Rides: </h3>
+
+        <h3>Number of Rides: </h3>
+
+        <h3>Number of Rides: </h3>
+    </section>
+
+    <button>Request a Ride</button>
+
+    <footer class="footer">
+        <!-- website footer -->
+        <img src="" alt="">
+
+        <div class="footerExplore">
+            <h5>Explore</h5>
+
+            <li>
+                <ul>Home</ul>
+                <ul>Share a Ride</ul>
+                <ul>Request a Ride</ul>
+            </li>
+        </div>
+
+        <div class="footerUserDashboard">
+            <h5>User Dashboard</h5>
+
+            <li>
+                <ul>Driver Dashboard</ul>
+                <ul>Ride History</ul>
+            </li>
+        </div>
+
+        <div class="supportUs">
+            <h5>Support Us</h5>
+
+            <li>
+                <ul>Contact Us</ul>
+                <ul>About Us</ul>
+                <ul>Book a Ride</ul>
+            </li>
+        </div>
+
+        <p>© 2026 CarpoolConnect. All rights reserved. <br>
+            This website is developed for educational purposes only.
+        </p>
+    </footer>
+
+    <script src=".\RequestPage.js"></script>
+</body>
+</html>
