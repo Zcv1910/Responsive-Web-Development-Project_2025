0a5f658006f60e01c5c236e407266d7c36d9abb2
Lianhanjun <lianhanjun1@gmail.com>
2026-03-16 15:17:46 +0800

    Setting page

---
+++ RWDD_Project(2026)/Insert Account_Details.php
@@ -0,0 +1,9 @@
+<?php 
+
+$_Username = $_POST['Username'];
+$_PhoneNumber = $_POST['Phone Number']; 
+$_Email = $_POST['Email'];
+$_Password = $_POST['Password'];
+$_ConfirmPassword = $_POST['Confirm Password'];
+
+?>
